705ac1f2626be0c84043d1bb0eda2cf03b520c9b
Eric Bailey <eric@ericb.me>
2023-12-05 02:23:04 -0600

    feat(hcat): start parsings args

--- Chapter08/hcat/src/HCat.hs
+++ Chapter08/hcat/src/HCat.hs
@@ -1,4 +1,21 @@
+{-# LANGUAGE LambdaCase #-}
+
 module HCat where
 
+import qualified System.Environment as Env
+
 runHCat :: IO ()
-runHCat = return ()
+runHCat = handleArgs >>= displayMessage
+  where
+    displayMessage = \case
+      Left err -> putStrLn $ "Error: " <> err
+      Right fname ->
+        putStrLn $ "Opening file: " <> fname
+
+handleArgs :: IO (Either String FilePath)
+handleArgs = parseArgs <$> Env.getArgs
+  where
+    parseArgs = \case
+      [fname] -> Right fname
+      [] -> Left "no filename provided"
+      _ -> Left "multiple files not supported"
